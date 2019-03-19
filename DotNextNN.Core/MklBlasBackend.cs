4a870f70fdc1c8caab9d460c63445e5ea97a4c9a
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-03-19 16:44:36 +0300

    First stage. Matrices.

--- DotNextNN.Core/MklBlasBackend.cs
+++ DotNextNN.Core/MklBlasBackend.cs
@@ -4,6 +4,12 @@ namespace DotNextNN.Core
 {
     public class MklBlasBackend
     {
+        /// <summary>
+        /// https://software.intel.com/en-us/mkl-developer-reference-fortran-gemv
+        /// 
+        /// Computes a matrix-vector product using a general matrix.
+        /// y := alpha*A*x + beta*y
+        /// </summary>
         public unsafe void gemv(TransposeOptions trans, int m, int n, float alpha, float[] A, int lda, float[] x, int incx, float beta, float[] y, int incy)
         {
             fixed (float* pA = A, pX = x, pY = y)
@@ -12,6 +18,12 @@ namespace DotNextNN.Core
             }
         }
 
+        /// <summary>
+        /// https://software.intel.com/en-us/mkl-developer-reference-fortran-ger
+        /// 
+        /// Performs a rank-1 update of a general matrix.
+        /// A := alpha*x*y'+ A
+        /// </summary>
         public unsafe void ger(int m, int n, float alpha, float[] x, int incx, float[] y, int incy, float[] A, int lda)
         {
             fixed (float* px = x, py = y, pA = A)
@@ -20,6 +32,12 @@ namespace DotNextNN.Core
             }
         }
 
+        /// <summary>
+        /// https://software.intel.com/en-us/mkl-developer-reference-fortran-axpy
+        /// 
+        /// Computes a vector-scalar product and adds the result to a vector.
+        /// y := a*x + y
+        /// </summary>
         public unsafe void axpy(int n, float alpha, float[] x, int incx, float[] y, int incy)
         {
             fixed (float* px = x, py = y)
@@ -28,6 +46,12 @@ namespace DotNextNN.Core
             }
         }
 
+        /// <summary>
+        /// https://software.intel.com/en-us/mkl-developer-reference-fortran-gemm-1
+        /// 
+        /// Computes a matrix-matrix product with general integer matrices.
+        /// C := alpha*(op(A) + A_offset)*(op(B) + B_offset) + beta*C + C_offset 
+        /// </summary>
         public unsafe void gemm(TransposeOptions transA, TransposeOptions transB, int m, int n, int k, float alpha, float[] A, int lda, float[] B, int ldb, float beta, float[] C, int ldc)
         {
             fixed (float* pA = A, pB = B, pc = C)
