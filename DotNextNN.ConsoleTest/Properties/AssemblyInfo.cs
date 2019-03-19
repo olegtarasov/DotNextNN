63f05866092dcd5b4651eacf3fd225c5ab069bc3
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-03-19 15:46:09 +0300

    Renamed ConsoleTest

--- DotNextNN.ConsoleTest/Properties/AssemblyInfo.cs
+++ DotNextNN.ConsoleTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotNextNN.Console")]
+[assembly: AssemblyTitle("DotNextNN.ConsoleTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNextNN.Console")]
+[assembly: AssemblyProduct("DotNextNN.ConsoleTest")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
